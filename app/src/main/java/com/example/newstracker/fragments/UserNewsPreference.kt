b025f2100d5e7884c651afa89ca11ff983fa33eb
Neil <dellosaneil@gmail.com>
2020-12-24 22:29:12 +0800

    changed drop down menu values to names. use array index to determine value

--- app/src/main/java/com/example/newstracker/fragments/UserNewsPreference.kt
+++ app/src/main/java/com/example/newstracker/fragments/UserNewsPreference.kt
@@ -1,7 +1,7 @@
 package com.example.newstracker.fragments
 
 import android.os.Bundle
-import android.text.InputType
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -9,15 +9,17 @@ import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
 import com.example.newstracker.R
-import com.google.android.material.textfield.MaterialAutoCompleteTextView
+import com.google.android.material.button.MaterialButton
 import com.google.android.material.textfield.TextInputLayout
 
 class UserNewsPreference : Fragment() {
 
-    private var category : TextInputLayout? = null
-    private var language : TextInputLayout? = null
-    private var country : TextInputLayout? = null
+    private var category: TextInputLayout? = null
+    private var language: TextInputLayout? = null
+    private var country: TextInputLayout? = null
 
+
+    //country?.editText?.text RETRIEVE DATA
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -27,11 +29,32 @@ class UserNewsPreference : Fragment() {
         initializeCategoryDropDown(view)
         initializeLanguageDropDown(view)
         initializeCountryDropDown(view)
+
+        val trialButton = view.findViewById<MaterialButton>(R.id.news_saveButton)
+        trialButton.setOnClickListener { retrieveValue() }
+
         return view
     }
 
+    private fun indexNumber(key : String, array : Array<String>)  = array.indexOf(key)
+
+    private fun retrieveValue(){
+        val countryName = country?.editText?.text.toString()
+        val countryCode = resources.getStringArray(R.array.country_list_values)[indexNumber(countryName, resources.getStringArray(R.array.country_list_keys))]
+        val languageName = language?.editText?.text.toString()
+        val languageCode = resources.getStringArray(R.array.language_list_values)[indexNumber(languageName, resources.getStringArray(R.array.language_list_keys))]
+        val categoryCode = category?.editText?.text.toString()
+        saveToRoomDatabase(countryCode, languageCode, categoryCode)
+    }
+
+    private fun saveToRoomDatabase(countryCode: String?, languageCode: String?, categoryCode: String) {
+
+
+    }
+
+
     private fun initializeCountryDropDown(view: View?) {
-        val items = resources.getStringArray(R.array.country_list)
+        val items = resources.getStringArray(R.array.country_list_keys)
         country = view?.findViewById(R.id.news_country)
         val adapter = ArrayAdapter(requireContext(), R.layout.list_item, items)
         if (country != null) {
@@ -40,7 +63,7 @@ class UserNewsPreference : Fragment() {
     }
 
     private fun initializeLanguageDropDown(view: View?) {
-        val items = resources.getStringArray(R.array.language_list)
+        val items = resources.getStringArray(R.array.language_list_keys)
         language = view?.findViewById(R.id.news_language)
         val adapter = ArrayAdapter(requireContext(), R.layout.list_item, items)
         if (language != null) {
